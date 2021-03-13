6276a74d63bae8766247b2c1eee4beb81b1151c4
yuriipanchyshyn <yuriipanchyshyn1@gmail.com>
2021-03-13 11:01:29 +0200

    working with entities

--- src/main/java/com/filmreview/springbootproject/model/Film.java
+++ src/main/java/com/filmreview/springbootproject/model/Film.java
@@ -1,8 +1,48 @@
 package com.filmreview.springbootproject.model;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+
+@Entity
 public class Film {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
+
+    @NotBlank(message = "Tittle may not be blank.")
+    private String title;
+
+    @NotBlank(message = "Description may not be blank.")
     private String description;
 
+
+    public Film() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
