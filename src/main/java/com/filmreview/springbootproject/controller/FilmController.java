1d7e878ba1d079d1514ddc1236983edbad2fc223
yuriipanchyshyn <yuriipanchyshyn1@gmail.com>
2021-03-15 12:58:22 +0200

    working with business-logic

--- src/main/java/com/filmreview/springbootproject/controller/FilmController.java
+++ src/main/java/com/filmreview/springbootproject/controller/FilmController.java
@@ -4,14 +4,18 @@ import com.filmreview.springbootproject.model.Film;
 import com.filmreview.springbootproject.service.FilmService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 public class FilmController {
+    private final int ROW_PER_PAGE = 3;
     private final FilmService filmService;
 
     public FilmController(FilmService filmService) {
@@ -19,8 +23,17 @@ public class FilmController {
     }
 
     @GetMapping("/home")
-    public String findAll(Model model) {
-        model.addAttribute("films", filmService.findAll());
+    public String findAll(Model model,
+                          @RequestParam(value = "page", defaultValue = "1") int pageNumber) {
+        List<Film> films = filmService.findAll(pageNumber, ROW_PER_PAGE);
+        long count = filmService.count();
+        boolean hasPrev = pageNumber > 1;
+        boolean hasNext = (pageNumber * ROW_PER_PAGE) < count;
+        model.addAttribute("films", films);
+        model.addAttribute("hasPrev",hasPrev);
+        model.addAttribute("prev",pageNumber-1);
+        model.addAttribute("hasNext",hasNext);
+        model.addAttribute("next",pageNumber+1);
         return "home";
     }
 
@@ -33,7 +46,11 @@ public class FilmController {
 
     @PostMapping("/home/add-new-film")
     public String addFilm(Model model,
-                          @ModelAttribute("film") @Valid Film film) {
+                          @ModelAttribute("film") @Valid Film film,
+                          BindingResult bindingResult) {
+        if(bindingResult.hasErrors()) {
+            return "add_new_film";
+        }
         filmService.save(film);
         return "redirect:/home";
     }
--- src/main/java/com/filmreview/springbootproject/service/FilmService.java
+++ src/main/java/com/filmreview/springbootproject/service/FilmService.java
@@ -2,8 +2,12 @@ package com.filmreview.springbootproject.service;
 
 import com.filmreview.springbootproject.model.Film;
 import com.filmreview.springbootproject.repository.FilmRepository;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -14,14 +18,22 @@ public class FilmService {
         this.filmRepository = filmRepository;
     }
 
-    public List<Film> findAll() {
-        return filmRepository.findAll();
+    public List<Film> findAll(int pageNumber, int rowPerPage) {
+        List<Film> films = new ArrayList<>();
+        Pageable sortedByIdAsc = PageRequest.of(pageNumber-1, rowPerPage,
+                Sort.by("id").ascending());
+        filmRepository.findAll(sortedByIdAsc).forEach(films::add);
+        return films;
     }
 
     public void save(Film film) {
         if(film != null) {
             filmRepository.save(film);
         }
+    }
+
+    public long count() {
+        return filmRepository.count();
     }
 
 
