3c6cd92608bac26ca52b3c3f894c01174bc5857d
yuriipanchyshyn <yuriipanchyshyn1@gmail.com>
2021-04-14 14:07:49 +0300

    implemented EmailSender for project

--- src/main/java/com/filmreview/springbootproject/email/EmailSender.java
+++ src/main/java/com/filmreview/springbootproject/email/EmailSender.java
@@ -1,4 +1,6 @@
 package com.filmreview.springbootproject.email;
 
 public interface EmailSender {
+    void send(String to, String email);
 }
+
---
+++ src/main/java/com/filmreview/springbootproject/email/EmailService.java
@@ -0,0 +1,35 @@
+package com.filmreview.springbootproject.email;
+
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+@Service
+public class EmailService implements EmailSender{
+
+    private final JavaMailSender javaMailSender;
+
+    public EmailService(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    @Override
+    @Async
+    public void send(String to, String email) {
+        try{
+            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
+            helper.setText(email, true);
+            helper.setTo(to);
+            helper.setSubject("Confirm your email");
+            helper.setFrom("bystro@gmail.com");
+            javaMailSender.send(mimeMessage);
+        } catch(MessagingException e) {
+            throw new IllegalStateException("Failed to send email");
+        }
+    }
+}
