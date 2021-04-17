8f18af3c9a148143cf8e7f2dd2fa00dbfc331ee3
yuriipanchyshyn <yuriipanchyshyn1@gmail.com>
2021-04-17 22:48:38 +0300

    Refactor: add validation for RegistrationRequest

--- src/main/java/com/filmreview/springbootproject/model/RegistrationRequest.java
+++ src/main/java/com/filmreview/springbootproject/model/RegistrationRequest.java
@@ -2,9 +2,6 @@ package com.filmreview.springbootproject.model;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
